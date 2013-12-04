1860144ebd9a238a34cc2b7ae8544c94a9b34553
Sagar Mehta <mehta.sagarv@gmail.com>
2013-12-03 16:56:00 -0800

    update to take generic args, batch insert into db

--- process.py
+++ process.py
@@ -1,29 +1,60 @@
 from pymongo import MongoClient
 from pprint import pprint
 import json
+import sys
 
 #connect to db
 client = MongoClient('localhost', 27017)
 
 db = client.dataquick
 
-schemaFile = open('./data/avm.json' , 'r')
-dataFile = open('./data/SIDEKICK_AVM_TEST.txt', 'r')
+args = sys.argv
+
+print args
+
+if len(args) < 3:
+  print 'usage: python process.py <schema file> <data file>'
+  sys.exit()
+
+schemaFile = open(args[1] , 'r')
+dataFile = open(args[2], 'r')
 
 schema = schemaFile.read()
 
-print schema
 schemaJson = json.loads(schema)
-
+mongoCollection = db[schemaJson['name']]
 pprint(schemaJson)
+batch = []
+batchIndex = 0
+totalIndex = 0
 
 while True:
   line = dataFile.readline()
   if line == '':
-    break
-  print line
-  index = 0
+    break  
+  lineIndex = 0
+  record = {}
+
   for field in schemaJson['fields']:
     width = field['width']
-    print field['name'] + ' ' + line[index:index+width-1]
-    index+=width
+    value = line[lineIndex:lineIndex+width-1].strip()
+
+    if field['type'] == 'number':
+      value = float(value)
+
+    record[field['name']] = value
+    lineIndex+=width
+
+  batch.append(record)
+  batchIndex += 1
+
+  if batchIndex == 1000:
+    totalIndex += batchIndex
+    print 'batch complete', totalIndex
+    mongoCollection.insert(batch, w=0)
+    batch = []
+    batchIndex = 0
+
+#do the last insert
+if len(batch):
+  mongoCollection.insert(batch, w=0)
